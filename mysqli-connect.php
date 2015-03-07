75630deada9ca7b4ebb92c5b7a7eb16cc2b03887
James Van Waza <jgacuca@gmail.com>
2015-03-07 18:52:51 -0500

    Changed database name to soccer

--- mysqli-connect.php
+++ mysqli-connect.php
@@ -1,24 +1,24 @@
 <?php
-	/*
-	This file provides the information for accessing the database sand coonnecting to MySQL. It also sets the language coding to utf-8
-*/
+/*
+This file provides the information for accessing the database sand coonnecting to MySQL. It also sets the language coding to utf-8
+ */
 
-	//First we define the constants:
-	define('DB_USER', 'admin9997');
-	define('DB_PASSWORD', 'WP6cV+~SqUU~w#Nzu9');
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'simpleIDB');
+//First we define the constants:
+define('DB_USER', 'admin9997');
+define('DB_PASSWORD', 'WP6cV+~SqUU~w#Nzu9');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'soccer');
 
-	/*
+/*
 
-	/*
-	Next we assign the database connection to a variable that we will call $dbcon
-	*/
-	$dbcon = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MYSQL: ' . mysqli_connect_error());
+/*
+Next we assign the database connection to a variable that we will call $dbcon
+ */
+$dbcon = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MYSQL: ' . mysqli_connect_error());
 
-	/*
-	Finally, we set the language encoding as utf-8
-	*/
+/*
+Finally, we set the language encoding as utf-8
+ */
 
-	mysqli_set_charset($dbcon, 'utf8');
+mysqli_set_charset($dbcon, 'utf8');
 ?>
